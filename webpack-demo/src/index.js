2d4acf7be4c365ef4bc77de4975b107675622a7b
Saito, Shugo | Shugo | COED <shugo.saito@rakuten.com>
2017-08-09 13:48:20 +0900

    - Output Management

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,22 +1,28 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+// import './style.css';
+// import Icon from './icon.png';
+// import Data from './data.xml';
+import printMe from './print.js';
 
 
 function component() {
   var element = document.createElement('div');
+  var btn = document.createElement('button');
 
   // Lodash, currently included via a script, is required for this line to work
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
+  // element.classList.add('hello');
 
   // Add the image
-  var myIcon = new Image();
-  myIcon.src = Icon;
-  element.appendChild(myIcon);
+  // var myIcon = new Image();
+  // myIcon.src = Icon;
+  // element.appendChild(myIcon);
 
-  console.log(Data);
+  // console.log(Data);
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
+  element.appendChild(btn);
 
   return element;
 }
